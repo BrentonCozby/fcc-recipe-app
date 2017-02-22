e0efe820346ec91d8671245b3e976e2350434ad8
Brenton Cozby <brenton.cozby@gmail.com>
2017-02-22 13:02:40 -0500

    add server and build for production

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// Static files
+app.use(express.static('./build'))
+
+// Serve index.html
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+// Start the server
+const PORT = 4007
+app.listen(PORT, function() {
+    console.log(`Listening on ${PORT}...`)
+})
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import AppContainer from './containers/AppContainer.js';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/projects/FreeCodeCamp/fcc-recipe-app'>
       <Route component={AppContainer}/>
   </Router>,
   document.getElementById('root')

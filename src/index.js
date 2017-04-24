577fff1d4f145998c8de01286e806a77f3fe2c03
Brenton Cozby <brenton.cozby@gmail.com>
2017-04-24 10:01:03 -0400

    edit homepage route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import AppContainer from './containers/AppContainer.js';
 import './index.css';
 
 ReactDOM.render(
-  <Router basename='/projects/FreeCodeCamp/fcc-recipe-app'>
+  <Router basename='/free-code-camp/fcc-recipe-app'>
       <Route component={AppContainer}/>
   </Router>,
   document.getElementById('root')
